17b0475bd7ae292d510cbbb6cc86d8a9ee329a4f
Adam Kanigowski <ka31544@zut.edu.pl>
2019-01-06 20:20:05 +0100

    Dodane szablonowanie, Sass, podstawowe trasowanie i kontrolery

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -6,10 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class MainController extends Controller
+class LoginController extends Controller
 {
     public function indexAction(Request $request)
     {
-        return $this->render('main/main.html.twig');
+        return $this->render('main/login.html.twig');
     }
 }
