014b8118ee520f6bc465e4e5734d29e2c8c05454
Adam Kanigowski <ka31544@zut.edu.pl>
2019-01-12 22:58:57 +0100

    Update form and templates Twig

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -13,7 +13,25 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('login')->add('typeAccount')->add('active');
+        $builder->add('login', 'text', array(
+            'required' => true,
+            'label' => 'Login',
+            'attr' => array(
+                'class' => 'is-large'
+            )
+        ))->add('typeAccount', 'text', array(
+            'required' => true,
+            'label' => 'Typ konta',
+            'attr' => array(
+                'class' => 'is-large'
+            )
+        ))->add('active', 'text', array(
+            'required' => true,
+            'label' => 'Aktywność',
+            'attr' => array(
+                'class' => 'is-large'
+            )
+        ));
     }/**
      * {@inheritdoc}
      */
